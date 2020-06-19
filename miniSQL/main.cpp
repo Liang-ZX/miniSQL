8429335027383a585b25377297f81671917523ef
liangzx <3170105151@zju.edu.cn>
2020-06-19 20:03:55 +0800

    add main.cpp

---
+++ miniSQL/main.cpp
@@ -0,0 +1,49 @@
+#include "interpreter.h"
+#include "buffermanager.h"
+#include "CatalogManager.h"
+
+BufferManager buffer_manager("testrecord");
+CatalogManager catalog_manager;
+int main()
+{
+	Interpreter interpreter(nullptr, &catalog_manager);
+	cout << "***********************MiniSQL***************************" << endl;
+	bool readFile = false;
+	ifstream file;
+	string query;
+	while (true)
+	{
+		if (readFile) {
+			file.open(interpreter.execFile);
+			if (!file.is_open()) {
+				cout << "Fail to open file '" << interpreter.execFile << "'." << endl;
+				readFile = false;
+				continue;
+			}
+			while (getline(file, query, ';'))
+			{
+				int status = interpreter.interprete(query);
+				if (status == 200)
+					break; //quit
+			}
+			file.close();
+			readFile = 0;
+			interpreter.execFile = "";
+		}
+		else {
+			cout << "minisql>>>";
+			getline(cin, query, ';');
+			int status = interpreter.interprete(query);
+			if (status == 200) {
+				break;
+			}
+			else if (status == 2) {
+				readFile = true;
+			}
+			else {
+				cout << "query OK" << endl;
+			}
+		}
+	}
+	return 0;
+}
